e3f6dd186f5a8906881abde39583b6b9ddb7a6d3
notserpp <prestonsegura@yahoo.com>
2018-10-24 14:37:21 -0700

    template page

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var http = require('http');
+var fs = require('fs');
+
 
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    fs.readFile('index.html', function(err,data)
+    {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+    });
 }).listen(process.env.PORT);
