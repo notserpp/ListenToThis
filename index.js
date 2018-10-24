dff09984067366b3b70ec9bde4305aa73828d978
notserpp <prestonsegura@yahoo.com>
2018-10-24 16:38:06 -0700

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 
 http.createServer(function (request, response) {
-    response.sendFile(path.join(__dirname, '../public', 'index.html'));
+    response.sendFile(path.join(__dirname, '/public', 'index.html'));
     response.writeHead(200, {'Content-Type': 'text/plain'});
 }).listen(process.env.PORT);
 
